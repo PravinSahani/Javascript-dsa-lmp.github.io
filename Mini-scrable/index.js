905f7c2a89814b5cba8b205fe852c4a9cb0ac4b0
PravinSahani <pravinkumarsahani7@gmail.com>
2022-05-17 14:42:38 +0530

    mini-scrable

---
+++ Mini-scrable/index.js
@@ -0,0 +1,6 @@
+// JS code here
+function wordsLength(){
+
+    let inputVal = document.getElementById("evaluatedText").value;
+    document.getElementById("letterCount").innerHTML = inputVal.length;
+}
